e57b2aede158b4b457e76f2049c95ed7a6de3c41
Jayaram Kasi <jayaram@vonnue.com>
2025-01-07 17:54:36 +0530

    Minor change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true, copyDtsFiles: true })],
+  plugins: [react(), dts({ rollupTypes: false, copyDtsFiles: true })],
   test: {
     environment: 'jsdom',
     exclude: ['**/node_modules/**', '**/dist/**', '**/.stories.tsx'],

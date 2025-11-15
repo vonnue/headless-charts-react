c29681e93f397f22cb9ebd438166c2605ac04b9a
Jayaram Kasi <jayaram@vonnue.com>
2025-11-15 11:34:57 +0530

    Dangerously pushing new version

--- src/utils/mergeTailwindClasses.tsx
+++ src/utils/mergeTailwindClasses.tsx
@@ -6,7 +6,20 @@ function mergeTailwindClasses(...classStrings: (string | undefined | null)[]) {
     .map((str) => {
       str &&
         str.split(/\s+/g).map((token: string) => {
-          classHash[token.split('-')[0]] = token;
+          const parts = token.split('-');
+          const base = parts[0];
+
+          if (base === 'stroke' || base === 'fill') {
+            const secondPart = parts[1];
+
+            if (secondPart && !isNaN(Number(secondPart))) {
+              classHash[`${base}-width`] = token;
+            } else {
+              classHash[base] = token;
+            }
+          } else {
+            classHash[base] = token;
+          }
         });
     });
   return Object.values(classHash).sort().join(' ');

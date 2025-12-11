8b38beff0c9ed74c3ac3386497f9f3a6835cb001
Jayaram Kasi <jayaram@vonnue.com>
2025-12-11 11:58:41 +0530

    Minor bug fix

--- src/components/gauges/SpeedometerChart/speedometer.test.tsx
+++ src/components/gauges/SpeedometerChart/speedometer.test.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import SpeedometerChart from '.';
 
